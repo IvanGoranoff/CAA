f42ab2ef66be28a9cc0d57eaced9cb6f69d4090f
IvanGoranoff <94969526+IvanGoranoff@users.noreply.github.com>
2023-01-17 14:49:29 +0200

    Add files via upload

---
+++ LogicalFunctions/LogicalFunctions/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+static void Main(string[] args)
+{
+    LogicalFunctions logicalFunctions = new LogicalFunctions();
+    while (true)
+    {
+        Console.WriteLine("Enter command:");
+        string input = Console.ReadLine();
+        string[] inputList = input.Split(' ');
+        string command = inputList[0];
+        string[] parameters = inputList.Skip(1).ToArray();
+        switch (command)
+        {
+            case "DEFINE":
+                logicalFunctions.DefineFunction(string.Join(" ", parameters));
+                break;
+            case "SOLVE":
+                Console.WriteLine(logicalFunctions.SolveFunction(parameters[0], parameters.Skip(1).Select(bool.Parse).ToArray()));
+                break;
+            case "SAVE":
+                logicalFunctions.SaveFunctions(parameters[0]);
+                break;
+            case "LOAD":
+                logicalFunctions.LoadFunctions(parameters[0]);
+                break;
+            case "TRUTHTABLE":
+                logicalFunctions.PrepareTruthTable(parameters[0]);
+                break;
+            case "FIND":
+                logicalFunctions.FindFunction(parameters);
+                break;
+            case "EXIT":
+                return;
+            default:
+                Console.WriteLine("Invalid command.");
+                break;
+        }
+    }
+}
+
+class LogicalFunctions
+{
+    private Dictionary<string, LogicalFunction> functions;
+    private Dictionary<string, bool> operands;
+
+    public LogicalFunctions()
+    {
+        functions = new Dictionary<string, LogicalFunction>();
+        operands = new Dictionary<string, bool>();
+    }
+
+    // Define a new function
+    public void DefineFunction(string input)
+    {
+        // Parse function name and expression from input
+        int colonIndex = input.IndexOf(':');
+        string functionName = input.Substring(0, colonIndex).Trim();
+        string functionExpression = input.Substring(colonIndex + 1).Trim();
+
+        // Validate function expression
+        ValidateExpression(functionExpression);
+
+        // Create a new LogicalFunction object and add it to the functions dictionary
+        LogicalFunction function = new LogicalFunction(functionExpression);
+        functions.Add(functionName, function);
+    }
+
+    // Validate the expression
+    private void ValidateExpression(string expression)
+    {
+        // Iterate through each character in the expression
+        for (int i = 0; i < expression.Length; i++)
+        {
+            char c = expression[i];
+
+            // If the character is a letter, check if it's a defined operand or function
+            if (Char.IsLetter(c))
+            {
+                string operand = "";
+                while (i < expression.Length && Char.IsLetter(expression[i]))
+                {
+                    operand += expression[i];
+                    i++;
+                }
+            }
+        }
+    }
+
+    // Save functions to a file
+    public void SaveFunctions(string filePath)
+    {
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            foreach (var function in functions)
+            {
+                writer.WriteLine("{0}:{1}", function.Key, function.Value.Expression);
+            }
+        }
+    }
+
+    // Load functions from a file
+    public void LoadFunctions(string filePath)
+    {
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                DefineFunction(line);
+            }
+        }
+    }
+
+    // Solve a function for given parameters
+    public bool SolveFunction(string functionName, params bool[] operands)
+    {
+        // Look up the function by name
+        LogicalFunction function = functions[functionName];
+
+        // Set the operands for the function
+        function.SetOperands(operands);
+
+        // Return the result of the function
+        return function.Evaluate();
+    }
+
+    // Prepare a truth table for a logical function
+    public void PrepareTruthTable(string functionName)
+    {
+        // Look up the function by name
+        LogicalFunction function = functions[functionName];
+
+        // Prepare the truth table
+        function.PrepareTruthTable();
+
+        // Print the truth table
+        function.PrintTruthTable();
+    }
+
+    // Find a logical function
+    // Find a logical function
+    public void FindFunction(string[] truthTable)
+    {
+        // Create a list to store all possible functions
+        List<string> possibleFunctions = new List<string>();
+
+        // Iterate through the truth table
+        for (int i = 0; i < truthTable.Length; i++)
+        {
+            // Split the row into operands and result
+            string[] row = truthTable[i].Split(':');
+            bool[] operands = Array.ConvertAll(row[0].Split(','), bool.Parse);
+            bool result = bool.Parse(row[1]);
+
+            // Iterate through all stored functions
+            foreach (var function in functions)
+            {
+                // Set the operands for the function
+                function.Value.SetOperands(operands);
+
+                // Evaluate the function
+                bool evalResult = function.Value.Evaluate();
+
+                // If the evaluation result matches the given result, add the function to the list of possible functions
+                if (evalResult == result)
+                {
+                    possibleFunctions.Add(function.Key);
+                }
+            }
+        }
+
+        // Print the list of possible functions
+        Console.WriteLine("Possible functions for the given truth table:");
+        foreach (string function in possibleFunctions)
+        {
+            Console.WriteLine(function);
+        }
+    }
+
+    // Visualize a function as a tree structure
+    public void VisualizeFunction(string functionName)
+    {
+        // Look up the function by name
+        LogicalFunction function = functions[functionName];
+
+        // Visualize the function as a tree structure
+        function.Visualize();
+    }
+}
+
+class LogicalFunction
+{
+    private string expression;
+    private bool[] operands;
+
+    public string Expression { get { return expression; } }
+
+    public LogicalFunction(string expression)
+    {
+        this.expression = expression;
+    }
+
+    // Set the operands for the function
+    public void SetOperands(bool[] operands)
+    {
+        this.operands = operands;
+    }
+
+    // Evaluate the function
+    public bool Evaluate()
+    {
+        // Implement logic for evaluating the function using the operands
+        return false;
+    }
+
+    // Prepare the truth table
+    public void PrepareTruthTable()
+    {
+        // Implement logic for preparing the truth table for the function
+    }
+
+    // Print the truth table
+    public void PrintTruthTable()
+    {
+        // Implement logic for printing the truth table
+    }
+
+    // Visualize the function as a tree structure
+    public void Visualize()
+    {
+        // Implement logic for visualizing the function as a tree structure
+    }
+   
+ 
+  
+
+}
+
+
+
